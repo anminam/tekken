96d198d7879c80b31a3296d0759971f00fc62df8
Anminam <minamhunmin@gmail.com>
2020-03-13 10:31:43 +0900

    Updates

---
+++ precache-manifest.35c761415bb269be4c013e00be908d0e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee5499cb720ba785859b88207c06cf68",
+    "url": "/tekken/index.html"
+  },
+  {
+    "revision": "0e4350e7b1350f6715b8",
+    "url": "/tekken/static/css/main.2591c833.chunk.css"
+  },
+  {
+    "revision": "af5235a0f1d7a023c276",
+    "url": "/tekken/static/js/2.46d075d9.chunk.js"
+  },
+  {
+    "revision": "94b29ac6d0ec426932e82da99c88b81c",
+    "url": "/tekken/static/js/2.46d075d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e4350e7b1350f6715b8",
+    "url": "/tekken/static/js/main.85922dcd.chunk.js"
+  },
+  {
+    "revision": "59106cbeccab7cb7cb62",
+    "url": "/tekken/static/js/runtime-main.f2c7b606.js"
+  }
+]);
